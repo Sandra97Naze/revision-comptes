26c95249de451807e64321dd863661c63300915f
Sandra97Naze <caperun@outlook.fr>
2025-02-10 14:41:02 +0400

    Update route.ts

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,1 +1,23 @@
+import NextAuth from "next-auth"
+import { AuthOptions } from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
 
+export const authOptions: AuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID || "",
+      clientSecret: process.env.GOOGLE_SECRET || "",
+    }),
+  ],
+  pages: {
+    signIn: '/login',
+  },
+  callbacks: {
+    async session({ session, token, user }) {
+      return session
+    },
+  },
+}
+
+const handler = NextAuth(authOptions)
+export { handler as GET, handler as POST }
